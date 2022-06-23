9417f5d13f5f72fdd94277e8f169f374774715fa
Renato <renatomarquesteles@gmail.com>
2022-06-23 09:44:13 -0300

    adds Sidebar component

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,9 +1,14 @@
 import { Header } from '../components/Header';
+import { Sidebar } from '../components/Sidebar';
 
 export function Event() {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
       <Header />
+      <main className="flex flex-1">
+        <div id="video" className="flex-1" />
+        <Sidebar />
+      </main>
     </div>
   );
 }
