f8a7cf5bf47458eb0ef9b9432dfc4809438f728c
Renato <renatomarquesteles@gmail.com>
2022-06-24 20:06:57 -0300

    adds group hover to Lesson component

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -16,10 +16,10 @@ export function Lesson(props: LessonProps) {
   ); // Wednesday • June, 22nd • 7:00 p.m.
 
   return (
-    <a href="#">
+    <a href="#" className="group">
       <span className="text-gray-300">{availableDateFormatted}</span>
 
-      <div className="rounded border border-gray-500 p-4 mt-2">
+      <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500">
         <header className="flex items-center justify-between">
           {isLessonAvailable ? (
             <span className="flex items-center gap-2 text-sm text-blue-500 font-medium">
