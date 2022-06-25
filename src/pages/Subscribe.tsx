b4d63b80d47530bb8be1d8154d25e03ee0b78298
Renato <renatomarquesteles@gmail.com>
2022-06-25 13:24:40 -0300

    adds Subscribe page

---
+++ src/pages/Subscribe.tsx
@@ -0,0 +1,53 @@
+import { Logo } from '../components/Logo';
+
+export function Subscribe() {
+  return (
+    <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
+      <div className="w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
+        <div className="max-w-[640px]">
+          <Logo />
+
+          <h1 className="mt-8 text-[2.5rem] leading-tight">
+            Build an{' '}
+            <strong className="text-blue-500">entire application</strong>, from
+            scratch, using <strong className="text-blue-500">React</strong>
+          </h1>
+
+          <p className="mt-4 text-gray-200 leading-relaxed">
+            In just one week you will master one of the most popular
+            technologies with high demand to access the best opportunities on
+            the space.
+          </p>
+        </div>
+
+        <div className="p-8 bg-gray-700 border border-gray-500 rounded">
+          <strong className="text-2xl mb-6 block">
+            Subscribe now for FREE!
+          </strong>
+
+          <form action="" className="flex flex-col gap-2 w-full">
+            <input
+              className="bg-gray-900 rounded px-5 h-14"
+              type="text"
+              placeholder="Your full name"
+            />
+            <input
+              className="bg-gray-900 rounded px-5 h-14"
+              type="email"
+              placeholder="Your email address"
+            />
+
+            <button
+              type="submit"
+              className="mt-4 bg-green-500 uppercase py-4 rounded font-bold text-sm hover:bg-green-700 transition-colors"
+            >
+              Grant your access
+            </button>
+          </form>
+        </div>
+      </div>
+
+      <img src="/src/assets/code-mockup.png" className="mt-10" alt="" />
+    </div>
+  );
+}
